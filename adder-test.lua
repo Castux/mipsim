3c3f351261011c489a9940b504a515ced5536cfe
Noé Falzon <noe@yousician.com>
2019-11-08 15:57:58 +0200

    Incr-decr

--- adder-test.lua
+++ adder-test.lua
@@ -1,23 +1,72 @@
 local Geom = require "Geom"
 local Simulator = require "Simulator"
 
-local geom = Geom()
+local function adder()
 
-local adder = io.open("examples/4bits-adder.txt"):read("a")
+	print("Testing adder...")
 
-geom:loadTiles(adder)
+	local geom = Geom()
+	local adder = io.open("examples/8bits-adder.txt"):read("a")
+	geom:loadTiles(adder)
+	local sim = Simulator(geom)
+	sim:setup()
 
-local sim = Simulator(geom)
-sim:setup()
+	for i = 0,255 do
+		print(i)
+		for j = 0,255 do
+			sim:setNumber("in_a", i)
+			sim:setNumber("in_b", j)
 
-for i = 0,15 do
-	for j = 0,15 do
-		sim:setNumber("in_a", i)
-		sim:setNumber("in_b", j)
+			local result = sim:readNumber("out")
+			local err = result % 256 ~= (i+j) % 256
 
-		local result = sim:readNumber("out")
-		local err = result % 16 ~= (i+j) % 16
-
-		print(i, "+", j, "=", result, err and "WRONG" or "")
+			if err then
+				print(i, "+", j, "=", result, err and "WRONG" or "")
+			end
+		end
 	end
+
+	print "Done."
 end
+
+local function incr()
+
+	print "Testing incr/decr..."
+
+	local geom = Geom()
+	local adder = io.open("examples/incr-decr.txt"):read("a")
+	geom:loadTiles(adder)
+	local sim = Simulator(geom)
+	sim:setup()
+
+	sim:setNumber("in_incr", 1)
+
+	for i = 0,255 do
+
+		sim:setNumber("in_a", i)
+		local result = sim:readNumber("out")
+		local err = result % 256 ~= (i+1) % 256
+
+		if err then
+			print(i, "+1", "=", result, err and "WRONG" or "")
+		end
+	end
+
+	sim:setNumber("in_incr", 0)
+
+	for i = 0,255 do
+
+		sim:setNumber("in_a", i)
+		local result = sim:readNumber("out")
+		local err = result % 256 ~= (i-1) % 256
+
+		if err then
+			print(i, "-1", "=", result, err and "WRONG" or "")
+		end
+	end
+
+	print "Done"
+end
+
+adder()
+incr()
